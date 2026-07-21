5282f8689510cf3695928dd0c3eccc5254f69a1c
ksoares1 <ksoares1@lenovo.com>
2026-07-21 13:31:47 -0300

    feat: create service for images upload

---
+++ src/services/storage.py
@@ -0,0 +1,37 @@
+import cloudinary.uploader
+from fastapi import UploadFile
+from src.core import cloudinary_config
+from fastapi import HTTPException, status
+
+ALLOWED_TYPES = {
+    "image/png",
+    "image/jpeg",
+    "image/webp"
+}
+
+async def upload_image(file: UploadFile) -> str: 
+    if not file.content_type or not file.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=400,
+            detail="File must be an image."
+        )
+    
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status_code=400,
+            detail="File type not allowed. Allowed types: PNG, JPEG, WEBP."
+        )
+    try:
+        result = cloudinary.uploader.upload(
+            file.file,
+            folder="products",
+            overwrite=False,
+            resource_type="image",
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error uploading image: {str(e)}"
+        )
+
+    return result['secure_url']
